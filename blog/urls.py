394afa40647522d85b70f840f15fe71de6a32716
AngelRomero <asebastianbrito@gmail.com>
2025-09-27 17:56:24 -0400

    updater done

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import ( PostListView,
+                    PostDetailView, 
+                    PostCreateView,
+                    PostUpdateView 
+                    )
 from . import views
 
 urlpatterns = [ # lista de funciones que se pueden llamar desde la ur
@@ -7,4 +11,5 @@ urlpatterns = [ # lista de funciones que se pueden llamar desde la ur
     path('posts/', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-new'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 
 
@@ -21,3 +21,11 @@ class PostCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'body', 'author']    
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post-update.html'
+    fields = ['title', 'body', 'author']
+
+    def get_success_url(self):
+        return f"/post/{self.object.pk}/"   
